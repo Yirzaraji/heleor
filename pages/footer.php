b1b5e0a65bddf4b1b8d54f6bd17dd6b9feccc5dc
Humphrey <humphrey.preston11@gmail.com>
2019-12-12 14:14:20 +0100

    Push Footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,1 +1,64 @@
-<div class="phpfooter">footer</div>
+<div class="footer">
+                <div class="contact">
+                        <div class="logo-foot">
+                                <img class="logo" src="images/photo contact.jpg">
+                        </div>
+                        <div class="question">
+                                UNE QUESTION ??
+                        </div>
+                        <div class="barre">
+
+                        </div>
+                        <div class="foot2">
+                                N'hésitez pas à prendre contact avec nous ...
+                        </div>
+                        <div class="barre2">
+
+                        </div>
+                        <button class="boutfoot">
+                                <b>NOUS CONTACTER</b>
+                        </button>
+                </div>
+        </div>
+        <div class="footer2">
+                <div class="footleft">
+                        <div>Blog</div>
+                        <div>Media</div>
+                        <div>Boutique</div>
+                        <div>Concept</div>
+
+                </div>
+
+                <div class="footcenter">
+                        <div class="picstop">
+                                <img class="imgtop" src="images/Reparacteurs.png">
+                                <img class="imgtop" src="images/Facebook.png">
+                                <img class="imgtop" src="images/Instagram.png">
+                        </div>
+                        
+                        <div class="textmid">2 ans de garantie - Livraison Offerte</div>
+                        <div class="textmid2"><b>Paiement sécurisé</b></div>
+                
+                        <div class="picsbot">   
+                                <img class="imgbot" src="images/SSL.png">
+                                <img class="imgbot" src="images/Visa.png">
+                                <img class="imgbot" src="images/Carte bleue.jpg">
+                                <img class="imgbot" src="images/Mastercard.png">
+                                <img class="imgbot" src="images/Maestro.png">
+                                <img class="imgbot" src="images/American Express.png">
+                                <img class="imgbot" src="images/Paypal.png"></div>
+                </div>
+
+                <div class="footright">
+                        <div>Rénovation</div>
+                        <div>Remerciements</div>
+                        <div>Réseaux Sociaux</div>
+                        <div>Accueil</div>
+                </div>
+
+        </div>
+        <div class="linebot">      
+        </div>
+        <div class="botbot">
+                © 2019 Heleor - contact@heleor.fr - Nos conditions générales de vente - Contact
+        </div>
