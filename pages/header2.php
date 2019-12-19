6dcc6f3297dfda1ce0d6cea1840a652d978d1bc8
Yirzaraji <not.iremy@gmail.com>
2019-12-19 16:56:57 +0100

    Jub Jub

--- pages/header2.php
+++ pages/header2.php
@@ -28,25 +28,25 @@
 
 <nav class="navmobile">
 	<div class="change_enfr">
-	<a href="index.php"><img src="../images/drapeaufr.png"></a>
+	<a href="../index.php"><img src="../images/drapeaufr.png"></a>
 	FR&nbsp;&nbsp;&nbsp;
-	<a href="uk/index.php"><img src="../images/drapeauen.png"></a>
+	<a href="../uk/index.php"><img src="../images/drapeauen.png"></a>
 	EN
 	</div>
 	
 	<div class="menublock03">
-	<span><a href="index.php">accueil</a></span> <span><a href="https://www.heleor.fr/postes-radio-bluetooth">boutique</a></span>
-	<span><a href="pages/renovation.php">renovation</a></span> 
+	<span><a href="../index.php">accueil</a></span> <span><a href="https://www.heleor.fr/postes-radio-bluetooth">boutique</a></span>
+	<span><a href="renovation.php">renovation</a></span> 
 
 
 	<div class="menulogo">
-	<a href="index.php"><img src="../images/logo.png"></a>
+	<a href="../index.php"><img src="../images/logo.png"></a>
 	</div>
 
 	
-	<span><a href="pages/concept.php">concept</a></span>
+	<span><a href="concept.php">concept</a></span>
 	<span><a href="https://www.heleor.fr/blog">blog</a></span>
-	<span><a href="pages/contact.php">contact</a></span>
+	<span><a href="contact.php">contact</a></span>
 	</div>
 	<div class="menureseau">
 	<a href="http://facebook.com"><img src="../images/fb.png"></a>
