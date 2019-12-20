d0c36fed00a7599e89ca2907ae0cc0d3ff8d0afc
Yirzaraji <not.iremy@gmail.com>
2019-12-20 13:59:02 +0100

    Update H1 + FIX CGV + Update UK site

---
+++ uk/pages/burger.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster|Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet"> 
+    <title>Heleor radio vintage</title>
+  </head>
+  <body>
+
+
+<div class="nav-wrapper">
+	<nav class="navbar navbar-expand-lg navbar-toggleable-xs navbar-toggleable-sm navbar-toggleable-md">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                                </button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item"><a href="" class="nav-link">Home</a></li>
+				<li class="nav-item"><a href="" class="nav-link">Products</a></li>
+				<li class="nav-item"><a href="" class="nav-link">Gallery</a></li>
+
+				<li class="nav-item dropdown">
+					<a href="#" class="dropdown-item " id="company-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Company <span class="fas fa-caret-down"></span> </a>
+					<ul role="menu" class="dropdown-menu dropdown-parent">
+						<a class="dropdown-item dropdown-child" href="">About Us</a>
+						<a class="dropdown-item dropdown-child" href="">Management</a>
+					</ul>
+				</li>
+
+				<li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+				<li class="nav-item"><a href="#" class="nav-link">Pages</a></li>
+				<li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+			</ul>
+		</div>
+	</nav>
+</div>
+
+
+</body>
+
+</html>
---
+++ uk/pages/renovation.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+
+    <title>Rénovation de votre poste !</title>
+ 
+
+</head>
+
+
+
+
+<body>
+
+<header> 
+	<?php include('header2.php');?>
+			 
+</header>
+
+   <div class="Renotitle">Rénovation de votre poste !</div>
+
+    <div class="totaltop">
+        <div class="center">
+            <div>Vous possédez une vieille radio ? Nous pouvons la <span class="under">RÉHABILITER</span> pour qu'elle
+                chante à nouveau.</div>
+            <div>Votre poste sera équipé de notre amplificateur <span class="under">BLUETOOTH</span> ainsi que des
+                fonctionnalités suivantes :</div>
+            <br>
+            <ul class="under">
+                <li>Haut parleur large bande 50w</li>
+                <li>Radio FM</li>
+                <li>Égaliseur numérique</li>
+                <li>Lecture MP3 USB/Carte Micro SD</li>
+                <li>Télécommande</li>
+                <li>Réglage Graves/Aigus</li>
+                <li>Eclairage du cadran</li>
+                <li>Isolation phonique</li>
+            </ul>
+            <div>Allumage et réglage du son par le bouton d'origine.</div>
+            <div>Afin de préserver le look <span class="under">VINTAGE</span> de votre poste, les composants comme la
+                radio FM, l'USB ou
+                mini-jack sont implantés à l'arrière du poste. Une plaque en bois vient remplacer la plaque arrière du
+                poste
+                afin d'<span class="under">OPTIMISER</span> le rendu sonore.</div>
+            <div>Les frais d'envoi sont à votre charge, la <span class="under">RÉ-EXPÉDITION</span> ainsi qu'une <span
+                    class="under">CARTE MICROsd</span> de 8go sont
+                <span class="under">OFFERTES</span></div>
+            <div>La réhabilitation de base coûte environ 500€.</div>
+            <div>Chaque poste est <span class="under">UNIQUE</span> la réhabilitation l'est aussi. Pour évaluer votre
+                poste, nous aurons besoin de
+                photos que vous pouvez nous transmettre par email à l'adresse contact@heleor.fr<br>
+                Nous vous établirons un devis dans les plus brefs délais. </div>
+        </div>
+    </div>
+
+    <div class="banrepa">
+
+        <div style="font-size: 30px">Nous rachetons aussi vos radios !</div>
+        <div>Si vous avez une radio dont vous ne vous servez plus, nous pouvons vous la racheter.</div>
+
+
+
+
+
+    </div>
+    
+    <div class="totalbot">
+        <div class="textreno">Nos postes vintage bluetooth</div>
+
+        <div class="caroubot backblack">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner backblack">
+                    <div class="carousel-item active backblack">
+                        <img class="d-block w-100 imgcarou" src="../images/Saba.png" alt="First slide">
+                        <div class="carousel-caption, caption backblack">
+                            <h3 class="caroucaption backblack">Saba</h3>
+                            <p class="caroudes backblack">&nbsp</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item backblack">
+                        <img class="d-block w-100 imgcarou" src="../images/Soneclair.png" alt="Second slide">
+                        <h3 class="caroucaption backblack">Soneclair</h3>
+                        <p class="caroudes backblack">&nbsp</p>
+                    </div>
+                    <div class="carousel-item backblack">
+                        <img class="d-block w-100 imgcarou" src="../images//Umeca.png" alt="Third slide">
+                        <h3 class="caroucaption backblack">Umeca</h3>
+                        <p class="caroudes backblack">&nbsp</p>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+<div class="titlerenotop under">
+
+Methode de rénovation
+
+</div>
+    <div class="description-bottom">
+
+        <br>
+        <div class="caissebot">
+            <div class="caisse">
+                <div class="textcaisse">
+                        <div class="titlereno under">La caisse <br></div>
+                        En parlant de caisse, certaines sont dans un sale état...Mauvais stockage,
+                    mauvaise utilisation (ce ne sont pas des étagères pour​​
+                    pot de fleurs !), usure du temps... Lorsque cela
+                    suffit, nous effectuons un simple nettoyage du poste.
+                    Cependant sur certains il est nécessaire de reprendre
+                    l'ébénisterie, poncer l'ancien vernis trop abîmé et
+                    revernir le poste. Nous nous efforçons de garder le
+                    rendu d'origine du poste, de ne pas le dénaturer.
+                    Malheureusement certains sont vraiment trop abîmés.
+                    Afin qu'ils ne finissent pas comme bois de chauffage,
+                    nous leur offrons une 2ème vie en les personnalisant
+                    entièrement.</div>
+            </div>
+            <div class="imgcaisse2"><img class="imgcaisse" src="../images/Interieur.jpg"></div>
+
+        </div>
+
+
+
+      
+            
+            <br>
+            <div class="boisbot">
+                <div class="bois">
+                    <div class="textbois">
+                            <div class="titlereno under">Le traitement du bois<br></div>
+                        Redonner vie à ces vieilles radios est aussi une
+                        démarche éco responsable. Et pour aller plus loin
+                        dans cette démarche, chaque caisse en bois est traitée
+                        contre les parasites avec des produits écologiques.
+                        Les produits non bio, utilisés couramment sont très
+                        toxiques et émanent pendant plusieurs années.
+                        Surtout le bois, une matière qui "boit" très bien les
+                        liquides et qui les restitue pendant plusieurs années.
+                        Utiliser des produits biologiques nous permet de
+                        travailler dans de meilleures conditions,
+                        sans émanations toxiques, et vous permet d'acquérir
+                        un produit qui ne diffusera pas des molécules toxiques
+                        chez vous.</div>
+                </div>
+                <div class="imgbois2"><img class="imgbois" src="../images/Caisse.jpg"></div>
+            </div>
+        
+
+
+
+
+                <br>
+<div class="tissubot">
+    <div class="tissu">
+                <div class="texttissu">
+                        <div class="under titlereno"> Le tissu<br></div>
+                        Certains postes ont un tissu qui recouvre le haut-parleur.
+                    Lorsqu'il n'est pas déchiré ou trop abîmé,
+                    nous l'enlevons délicatement du support pour le laver.
+                    Une fois sec et repassé, il retrouve sa place d'origine.
+                    D'autres nécessitent cependant d'être changés.
+                    Commence alors la recherche de la perle rare!
+                    Il faut trouver un tissu qui ne dénature pas
+                    l'esprit de la marque et du poste, qui fasse
+                    ressortir sa teinte tout en restant discret!
+                    Nous nous efforçons de trouver la pièce idéale
+                    pour chaque radio.</div>
+                </div>
+                <div class="imgtissu2"><img class="imgtissu" src="../images/tissu.jpg"></div>
+            </div>
+
+
+
+        </div>
+
+
+
+<footer>
+	<?php include('footer2.php');?>
+			
+</footer>
+        
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+        <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</body>
+
+</html>
